feb4d39f883d41d67914b8d3e353e03177f2f41f
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-21 20:46:26 +1100

    react router and view integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { Container } from "@mui/material";
 import "./App.css";
+import MainView from "./views/HomeView";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-	return <Container></Container>;
+	return (
+		<div className="flex flex-col p-5 h-screen w-screen">
+			<BrowserRouter>
+				<Navbar />
+				<Switch>
+					<Route exact path="/">
+						<MainView />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
