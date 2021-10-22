6bbc889bcc835018421be2440256a7200b09f604
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-22 23:45:53 +1100

    new font component

---
+++ src/components/fonts/ExoTwoFont.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ExoTwoFont.css";
+
+const ExoTwoFont: React.FC = ({ children }) => {
+	return <div className="font-catamaran">{children}</div>;
+};
+
+export default ExoTwoFont;
