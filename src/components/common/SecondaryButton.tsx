b9a328b02f9e60fd54d35c2ef6a5914f1c6d06d4
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-22 23:44:41 +1100

    common button component split to two button components

---
+++ src/components/common/SecondaryButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {
+	buttonText: string;
+	onClick?: () => void;
+}
+const SecondaryButton: React.FC<Props> = ({ buttonText, onClick }) => {
+	return (
+		<button
+			className="border border-green-500 text-green-500 p-3 rounded-sm hover:shadow-md font-medium hover:bg-green-500 hover:border-green-500 hover:text-white transition-all"
+			onClick={onClick}
+		>
+			{buttonText}
+		</button>
+	);
+};
+export default SecondaryButton;
