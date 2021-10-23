a8557ed74fea236cc78297fa50ab30ff7b19e70c
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-23 17:59:37 +1100

    added margin to navbar primary button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,21 +4,23 @@ import RouterLink from "./RouterLink";
 import LogoLink from "./LogoLink";
 
 const Navbar: React.FC = () => {
-	return (
-		<div className="flex justify-between 3xl:justify-evenly">
-			<div className="m-2 md:mx-0">
-				<LogoLink to="/" linkContent="HD" transition />
-			</div>
-			<div className="flex justify-end md:gap-5 items-center">
-				<div className="m-2 md:mx-0">
-					<RouterLink transition to="/about" linkText="About" />
-					<RouterLink transition to="/projects" linkText="Projects" />
-					<RouterLink transition to="/experience" linkText="Experience" />
-					<PrimaryButton buttonText="Contact" />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex justify-between 3xl:justify-evenly">
+      <div className="m-2 md:mx-0">
+        <LogoLink to="/" linkContent="HD" transition />
+      </div>
+      <div className="flex justify-end md:gap-5 items-center">
+        <div className="m-2 md:mx-0">
+          <RouterLink transition to="/about" linkText="About" />
+          <RouterLink transition to="/projects" linkText="Projects" />
+          <RouterLink transition to="/experience" linkText="Experience" />
+          <span className="m-1">
+            <PrimaryButton buttonText="Contact" />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
