593496d0e33683bca8ef576defbfc24a489aff4b
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-24 22:07:04 +1100

    new asset manager

---
+++ src/assets/assets.ts
@@ -0,0 +1,56 @@
+// Icons
+import bootstrapIcon from "./icons/bootstrap-icon.svg";
+import djangoIcon from "./icons/djangoproject-icon.svg";
+import expressIcon from "./icons/expressjs-icon.svg";
+import flutterIcon from "./icons/flutterio-icon.svg";
+import githubIcon from "./icons/github-brands.svg";
+import bashIcon from "./icons/bash-icon.svg";
+import menuIcon from "./icons/menu-icon.svg";
+import mongodbIcon from "./icons/mongodb-icon.svg";
+import nodejsIcon from "./icons/nodejs-icon.svg";
+import postgresIcon from "./icons/postgresql-icon.svg";
+import pythonIcon from "./icons/python-icon.svg";
+import rabbitmqIcon from "./icons/rabbitmq-icon.svg";
+import reactIcon from "./icons/reactjs-icon.svg";
+import tsIcon from "./icons/typescriptlang-icon.svg";
+import jsIcon from "./icons/javascript-icon.svg";
+import linkedinIcon from "./icons/linkedin-brands.svg";
+import closeIcon from "./icons/close-icon.svg";
+
+// Backgrounds
+import waveV1 from "./backgrounds/wave-haikei-v1.svg";
+import waveV2 from "./backgrounds/wave-haikei-v2.svg";
+import waveV3 from "./backgrounds/wave-haikei-v3.svg";
+import waveV4 from "./backgrounds/wave-haikei-v4.svg";
+
+// Site Logo (Special case)
+import { ReactComponent as Logo } from "./logo.svg";
+
+const Icons = {
+	bootstrapIcon,
+	djangoIcon,
+	expressIcon,
+	flutterIcon,
+	githubIcon,
+	bashIcon,
+	menuIcon,
+	mongodbIcon,
+	nodejsIcon,
+	postgresIcon,
+	pythonIcon,
+	rabbitmqIcon,
+	reactIcon,
+	tsIcon,
+	jsIcon,
+	linkedinIcon,
+	closeIcon,
+};
+
+const Backgrounds = {
+	waveV1,
+	waveV2,
+	waveV3,
+	waveV4,
+};
+
+export { Icons, Backgrounds, Logo };
