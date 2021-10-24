43db6f2f9c2185d195a32f1f03caa243b587950b
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-24 22:08:55 +1100

    new viewport context

---
+++ src/contexts/ViewportContext.tsx
@@ -0,0 +1,46 @@
+import React, { createContext, useEffect, useState } from "react";
+
+interface IViewportContext {
+	width: number;
+	height: number;
+}
+
+const ViewportContext = createContext<IViewportContext>({
+	width: window.innerWidth,
+	height: window.innerHeight,
+});
+
+const ViewportBreakpoints = {
+	mobile: 640,
+	tablet: 768,
+	laptop: 1024,
+	desktop: 1280,
+	ultrawide: 1920,
+};
+
+const ViewportProvider: React.FC = ({ children }) => {
+	const [width, setWidth] = useState<number>(0);
+	const [height, setHeight] = useState<number>(0);
+	const updateViewport = () => {
+		setWidth(window.innerWidth);
+		setHeight(window.innerHeight);
+	};
+
+	// Bind provider to 'resize' window event on mount and destroy on unmount
+	useEffect(() => {
+		window.addEventListener("load", updateViewport);
+		window.addEventListener("resize", updateViewport);
+		return () => {
+			window.removeEventListener("resize", updateViewport);
+			window.removeEventListener("load", updateViewport);
+		};
+	}, []);
+
+	return (
+		<ViewportContext.Provider value={{ width, height }}>
+			{children}
+		</ViewportContext.Provider>
+	);
+};
+
+export { ViewportContext, ViewportProvider, ViewportBreakpoints };
