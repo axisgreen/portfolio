e8d56324a36ac54ca7490eef97015a89bb983aa2
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-25 20:50:50 +1100

    added new view with ux fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,27 @@ import Footer from "./components/Footer/Footer";
 import PageBackground from "./components/common/PageBackground";
 import getRandomBackground from "./helpers/getRandomBackground";
 import { ViewportProvider } from "./contexts/ViewportContext";
+import ProjectView from "./views/ProjectView";
 function App() {
 	return (
-		<div>
-			<ViewportProvider>
-				<PageBackground imageUrl={getRandomBackground()}>
-					<BrowserRouter>
-						<div className="flex flex-col h-screen w-full text-white items-stretch px-4 py-4">
-							<Navigation />
-							<Switch>
-								<Route exact path="/">
-									<HomeView />
-								</Route>
-							</Switch>
-							<Footer />
-						</div>
-					</BrowserRouter>
-				</PageBackground>
-			</ViewportProvider>
-		</div>
+		<ViewportProvider>
+			<PageBackground imageUrl={getRandomBackground()}>
+				<BrowserRouter>
+					<div className="flex flex-col h-screen text-white px-4 py-4">
+						<Navigation />
+						<Switch>
+							<Route exact path="/">
+								<HomeView />
+							</Route>
+							<Route exact path="/projects">
+								<ProjectView />
+							</Route>
+						</Switch>
+						<Footer />
+					</div>
+				</BrowserRouter>
+			</PageBackground>
+		</ViewportProvider>
 	);
 }
 
