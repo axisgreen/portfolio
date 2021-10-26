37980b97a8b0254d1125da37fac607c0b4849da4
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-27 00:16:28 +1100

    ui fix

--- src/views/ProjectView.tsx
+++ src/views/ProjectView.tsx
@@ -4,14 +4,14 @@ import Navigation from "../components/Navigation/Navigation";
 
 const ProjectView: React.FC = () => {
 	return (
-		<div className="flex flex-col min-h-screen min-w-full text-white px-4 py-4">
+		<div className="flex flex-col min-h-screen min-w-full text-white p-4">
 			<Navigation />
 			<div className="flex flex-col flex-1 gap-8">
 				<div className="flex justify-center">
 					<div className="text-5xl font-semibold">Projects</div>
 				</div>
-				<div className="mx-auto mb-4">
-					<div className="flex flex-wrap bg-white shadow-xl rounded-md py-14 md:px-16 md:mx-16 justify-center">
+				<div className="mx-auto mb-4 xl:mb-0">
+					<div className="flex flex-wrap bg-gray-800 shadow-xl rounded-md py-14 md:px-16 md:mx-16 justify-center">
 						<Card title="" description="" image="" link="/card" />
 						<Card title="" description="" image="" link="/card" />
 						<Card title="" description="" image="" link="/card" />
