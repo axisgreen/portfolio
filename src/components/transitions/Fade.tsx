4ca13ed4bd65b77c99f776cb0af5075827ef3177
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-29 02:13:09 +1100

    renamed fadein to fade

--- src/components/transitions/Fade.tsx
+++ src/components/transitions/Fade.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { CSSTransition } from "react-transition-group";
-import "./FadeIn.css";
+import "./Fade.css";
 
 interface Props {
 	trigger: boolean;
 	timeout: number;
 }
 
-const FadeIn: React.FC<Props> = ({ trigger, timeout, children }) => {
+const Fade: React.FC<Props> = ({ trigger, timeout, children }) => {
 	return (
 		<CSSTransition
 			in={trigger}
 			timeout={timeout}
-			classNames="fade-in"
+			classNames="fade"
 			unmountOnExit
 		>
 			{children}
@@ -20,4 +20,4 @@ const FadeIn: React.FC<Props> = ({ trigger, timeout, children }) => {
 	);
 };
 
-export default FadeIn;
+export default Fade;
