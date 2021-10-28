58a0bc3b824b749c2a756669d11bb705e801b819
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-29 02:16:36 +1100

    card converted from vertical to horizontal

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -10,16 +10,18 @@ interface Props {
 const Card: React.FC<Props> = ({ title, description, image, link }) => {
 	return (
 		<NavLink exact to={link}>
-			<div className="w-72 h-auto flex flex-col justify-center p-2 m-2 hover:bg-white  hover:shadow-xl hover:text-black transition-all rounded-md border">
+			<div className="flex flex-col sm:flex-row justify-center p-4 m-2 hover:bg-white hover:shadow-xl hover:text-black transition-all rounded-md">
 				<img
 					src="https://via.placeholder.com/250x200"
 					alt="Card"
 					className="rounded-md"
 				/>
-				<div className="text-xl font-semibold mt-4 mb-2">Card Title</div>
-				<div>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-					vitae iure deserunt ratione velit!
+				<div className="flex flex-col px-8 py-4">
+					<div className="text-xl font-semibold mt-4 mb-2">Card Title</div>
+					<div>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+						vitae iure deserunt ratione velit!
+					</div>
 				</div>
 			</div>
 		</NavLink>
