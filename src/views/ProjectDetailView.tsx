a831eac49910ae305791c4745e6b27399a6c76fd
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-30 00:45:43 +1100

    dynamically adjust project info tile on unconventional screen heights

--- src/views/ProjectDetailView.tsx
+++ src/views/ProjectDetailView.tsx
@@ -5,8 +5,15 @@ import Footer from "../components/layout/Footer/Footer";
 import Navigation from "../components/layout/Navigation/Navigation";
 
 const ProjectDetailView: React.FC = () => {
+	const { height } = useContext(ViewportContext);
+	// Sets the min height for the project info tile if the screen is taller than convention
+	const minHeightForTallScreens =
+		height > 1080 ? "sm:min-h-63v" : "sm:min-h-80v";
 	return (
 		<div className="flex flex-1 flex-col-reverse lg:flex-row justify-center gap-8 3xl:gap-16 my-4 xl:mx-0 text-white">
+			<div
+				className={`bg-gray-800 shadow-xl rounded-md md:px-14 overflow-y-scroll inline self-center ${minHeightForTallScreens}  max-h-80v 3xl:min-h-63v 3xl:max-h-63v my-auto`}
+			>
 				<div className="lg:max-w-sm max-h-96">
 					<div className="flex flex-col gap-4 py-10 px-10 md:px-0">
 						<div className="text-3xl font-semibold">Project Name</div>
