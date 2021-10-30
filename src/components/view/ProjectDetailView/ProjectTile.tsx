f299a83816292a24fca5034ae90ed6ad33ffb8e0
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-31 00:58:35 +1100

    project info isolated to own component

---
+++ src/components/view/ProjectDetailView/ProjectTile.tsx
@@ -0,0 +1,61 @@
+import { useContext } from "react";
+import { Icons } from "../../../assets/assets";
+import { ViewportContext } from "../../../contexts/ViewportContext";
+import { IProject } from "../../../data/Projects";
+import openUrlInNewTab from "../../../helpers/openUrlInNewTab";
+import PrimaryButton from "../../common/PrimaryButton";
+import TechBadge from "../../common/TechBadge";
+import ViewSourceButton from "../../common/ViewSourceButton";
+
+interface Props {
+	project: IProject;
+}
+
+const ProjectTile: React.FC<Props> = ({ project }) => {
+	const { height } = useContext(ViewportContext);
+	// Sets the min height for the project info tile if the screen is taller than convention
+	const minHeightForTallScreens =
+		height > 1080 ? "sm:min-h-63v" : "sm:min-h-80v";
+
+	return (
+		<div
+			className={`bg-gray-800 shadow-xl rounded-md md:px-14 overflow-y-scroll inline self-center ${minHeightForTallScreens} max-h-80v 3xl:min-h-63v 3xl:max-h-63v my-auto`}
+		>
+			<div className="lg:max-w-sm max-h-96">
+				<div className="flex flex-col gap-4 py-10 px-10 md:px-0">
+					<div className="text-3xl font-semibold">{project.projectName}</div>
+					<div className="flex gap-1">
+						{project.technologiesUsed.map((technology, i) => {
+							return (
+								<TechBadge
+									key={i}
+									icon={technology.icon}
+									tooltip={technology.tooltip}
+								/>
+							);
+						})}
+					</div>
+					<div className="p-4 border rounded-md hover:bg-white hover:text-black transition-all">
+						Related to Work Experience
+					</div>
+					<div>{project.projectDescription}</div>
+
+					<PrimaryButton
+						buttonText="Live Demo"
+						buttonIcon={Icons.eyeIcon}
+						onClick={() => openUrlInNewTab(project.liveDemoLink)}
+					/>
+					{project.isOpenSource && (
+						<ViewSourceButton
+							buttonText="View Source"
+							buttonIcon={Icons.githubIcon}
+							onClick={() => openUrlInNewTab(project.openSourceRepo)}
+						/>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProjectTile;
--- src/views/ProjectDetailView.tsx
+++ src/views/ProjectDetailView.tsx
@@ -1,66 +1,24 @@
-import { useContext, useState } from "react";
-import { Icons } from "../assets/assets";
-import PrimaryButton from "../components/common/PrimaryButton";
-import TechBadge from "../components/common/TechBadge";
+import { useParams } from "react-router";
 import MobileShowcaseCarousel from "../components/view/ProjectDetailView/MobileShowcaseCarousel";
+import ProjectTile from "../components/view/ProjectDetailView/ProjectTile";
 import WebShowcaseCarousel from "../components/view/ProjectDetailView/WebShowcaseCarousel";
-import { ViewportContext } from "../contexts/ViewportContext";
+import getProjectFromId from "../helpers/getProjectFromId";
 
 const ProjectDetailView: React.FC = () => {
-	const { height } = useContext(ViewportContext);
-	// Sets the min height for the project info tile if the screen is taller than convention
-	const minHeightForTallScreens =
-		height > 1080 ? "sm:min-h-63v" : "sm:min-h-80v";
-
-	const [projectType, setProjectType] = useState<"mobile" | "web">("web");
+	const { projectId } = useParams<{ projectId: string }>();
+	const project = getProjectFromId(projectId);
 	return (
 		<div className="flex flex-1 flex-col-reverse lg:flex-row justify-center gap-8 3xl:gap-16 my-4 xl:mx-0 text-white">
-			<div
-				className={`bg-gray-800 shadow-xl rounded-md md:px-14 overflow-y-scroll inline self-center ${minHeightForTallScreens} max-h-80v 3xl:min-h-63v 3xl:max-h-63v my-auto`}
-			>
-				<div className="lg:max-w-sm max-h-96">
-					<div className="flex flex-col gap-4 py-10 px-10 md:px-0">
-						<div className="text-3xl font-semibold">Project Name</div>
-						<div className="flex gap-1">
-							<TechBadge icon={Icons.flutterIcon} tooltip="Flutter" />
-							<TechBadge icon={Icons.flutterIcon} tooltip="Flutter" />
-							<TechBadge icon={Icons.flutterIcon} tooltip="Flutter" />
-							<TechBadge icon={Icons.flutterIcon} tooltip="Flutter" />
-							<TechBadge icon={Icons.flutterIcon} tooltip="Flutter" />
-							<TechBadge icon={Icons.flutterIcon} tooltip="Flutter" />
-						</div>
-						<div className="p-4 border rounded-md hover:bg-white hover:text-black transition-all">
-							Related to Work Experience
-						</div>
-						<div>
-							Project Description - Lorem ipsum dolor sit amet consectetur
-							adipisicing elit. Nihil reiciendis ratione maxime doloribus
-							laborum. Incidunt minima quam in optio quas a laborum illo cumque
-							nesciunt, quis consequatur id, sed doloremque! Lorem ipsum dolor
-							sit amet, consectetur adipisicing elit. Incidunt voluptates
-							aspernatur maxime, consequuntur optio ratione adipisci commodi
-							labore eaque, ea doloribus explicabo molestiae repellat aut
-							possimus cum eveniet. Consectetur, voluptatem. Lorem ipsum dolor
-							sit amet consectetur adipisicing elit. Nihil reiciendis ratione
-							maxime doloribus laborum. Incidunt minima quam in optio quas a
-							laborum illo cumque nesciunt, quis consequatur id, sed doloremque!
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
-							voluptates aspernatur maxime, consequuntur optio ratione adipisci
-							commodi labore eaque, ea doloribus explicabo molestiae repellat
-							aut possimus cum eveniet. Consectetur, voluptatem.
-						</div>
-
-						<PrimaryButton buttonText="Live Demo" />
-					</div>
-				</div>
-			</div>
-			<div className="flex justify-center items-center">
-				{projectType === "mobile" ? (
-					<MobileShowcaseCarousel />
-				) : (
-					<WebShowcaseCarousel />
-				)}
-			</div>
+			{project && (
+				<>
+					<ProjectTile project={project} />
+					{project.projectTarget === "mobile" ? (
+						<MobileShowcaseCarousel />
+					) : (
+						<WebShowcaseCarousel />
+					)}
+				</>
+			)}
 		</div>
 	);
 };
