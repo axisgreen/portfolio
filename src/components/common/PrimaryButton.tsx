e322efd2e81649c16f5e614dadbabc307b3118b8
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-10-31 00:57:22 +1100

    primary and secondary buttons can have an icon

--- src/components/common/PrimaryButton.tsx
+++ src/components/common/PrimaryButton.tsx
@@ -2,17 +2,33 @@ import React from "react";
 
 interface Props {
 	buttonText: string;
+	buttonIcon?: string;
+
 	onClick?: () => void;
 }
 
-const PrimaryButton: React.FC<Props> = ({ buttonText, onClick }: Props) => {
+const PrimaryButton: React.FC<Props> = ({
+	buttonText,
+	buttonIcon,
+	onClick,
+}: Props) => {
 	// bg-gradient-to-l from-green-500 to-blue-500
 	return (
 		<button
 			className="bg-green-500 p-3 rounded-md hover:shadow-md text-white font-medium transition-all hover:bg-green-600"
 			onClick={onClick}
 		>
-			{buttonText}
+			<div className="flex gap-2 justify-center items-center">
+				{buttonIcon && (
+					<img
+						src={buttonIcon}
+						width="20px"
+						height="20px"
+						className="filter invert"
+					/>
+				)}
+				{buttonText}
+			</div>
 		</button>
 	);
 };
--- src/components/common/SecondaryButton.tsx
+++ src/components/common/SecondaryButton.tsx
@@ -2,15 +2,23 @@ import React from "react";
 
 interface Props {
 	buttonText: string;
+	buttonIcon?: string;
 	onClick?: () => void;
 }
-const SecondaryButton: React.FC<Props> = ({ buttonText, onClick }) => {
+const SecondaryButton: React.FC<Props> = ({
+	buttonText,
+	buttonIcon,
+	onClick,
+}) => {
 	return (
 		<button
 			className="border border-green-500 text-green-500 p-3 rounded-md hover:shadow-md font-medium hover:bg-green-500 hover:border-green-500 hover:text-white transition-all"
 			onClick={onClick}
 		>
-			{buttonText}
+			<div className="flex gap-2 justify-center items-center">
+				{buttonIcon && <img src={buttonIcon} width="20px" height="20px" />}
+				{buttonText}
+			</div>
 		</button>
 	);
 };
