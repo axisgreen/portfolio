1b9f18b1eed4d0511a3646cbde1e1d5a37d7d6b1
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-01 01:55:54 +1100

    updates to projects data model

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -6,15 +6,23 @@ type Technology = {
 		| "Bootstrap"
 		| "Django"
 		| "Python"
-		| "ExpressJS"
+		| "Express.js"
 		| "Bash"
 		| "MongoDB"
-		| "NodeJS"
+		| "Node.js"
 		| "PostgreSQL"
 		| "RabbitMQ"
-		| "ReactJS"
+		| "React.js"
 		| "TypeScript"
-		| "JavaScript";
+		| "JavaScript"
+		| "jQuery"
+		| "Amazon Web Services (AWS)"
+		| "Google Cloud Platform (GCP)"
+		| "DigitalOcean"
+		| "Vue.js"
+		| "Dart"
+		| "Stripe"
+		| "Heroku";
 	icon: string;
 };
 
@@ -55,10 +63,10 @@ const Projects: Project[] = [
 		aut possimus cum eveniet. Consectetur, voluptatem.`,
 		technologiesUsed: [
 			{ tooltip: "MongoDB", icon: Icons.mongodbIcon },
-			{ tooltip: "ExpressJS", icon: Icons.expressIcon },
-			{ tooltip: "ReactJS", icon: Icons.reactIcon },
+			{ tooltip: "Express.js", icon: Icons.expressIcon },
+			{ tooltip: "React.js", icon: Icons.reactIcon },
 			{
-				tooltip: "NodeJS",
+				tooltip: "Node.js",
 				icon: Icons.nodejsIcon,
 			},
 			{ tooltip: "TypeScript", icon: Icons.tsIcon },
@@ -75,14 +83,102 @@ const Projects: Project[] = [
 		id: "aims",
 		projectName: "AIMS (Agent Information Management System)",
 		projectDescription: "desc",
-		technologiesUsed: [{ tooltip: "Flutter", icon: Icons.flutterIcon }],
+		technologiesUsed: [
+			{ tooltip: "Python", icon: Icons.pythonIcon },
+			{ tooltip: "Django", icon: Icons.djangoIcon },
+			{ tooltip: "PostgreSQL", icon: Icons.postgresIcon },
+			{ tooltip: "Bootstrap", icon: Icons.bootstrapIcon },
+			{ tooltip: "JavaScript", icon: Icons.jsIcon },
+		],
 		projectTarget: "mobile",
 		media: ["screenshot.jpg", "video.gif"],
 		thumbnail: "thumbnail.jpg",
 		relatedWorkExperience: "Work Experience",
-		liveDemoLink: "https://livedemolink.com/",
+		liveDemoLink: "https://aims-demo.herokuapp.com/",
 		isOpenSource: false,
-		openSourceRepo: "https://github.com/someproject",
+		openSourceRepo: "https://github.com/axisgreen",
+	},
+	{
+		id: "servo",
+		projectName: "Servo",
+		projectDescription: "desc",
+		technologiesUsed: [
+			{ tooltip: "Python", icon: Icons.pythonIcon },
+			{ tooltip: "Django", icon: Icons.djangoIcon },
+			{ tooltip: "PostgreSQL", icon: Icons.postgresIcon },
+			{ tooltip: "Bootstrap", icon: Icons.bootstrapIcon },
+			{ tooltip: "jQuery", icon: Icons.jqueryIcon },
+			{ tooltip: "JavaScript", icon: Icons.jsIcon },
+			{ tooltip: "DigitalOcean", icon: Icons.digitaloceanIcon },
+		],
+		projectTarget: "web",
+		media: [],
+		thumbnail: "thumbnail.jpg",
+		relatedWorkExperience: "Work Experience",
+		liveDemoLink: "https://servoapp-demo.herokuapp.com/",
+		isOpenSource: false,
+		openSourceRepo: "https://github.com/axisgreen",
+	},
+	{
+		id: "phantom",
+		projectName: "Phantom",
+		projectDescription: "desc",
+		technologiesUsed: [
+			{ tooltip: "Python", icon: Icons.pythonIcon },
+			{ tooltip: "Django", icon: Icons.djangoIcon },
+			{ tooltip: "PostgreSQL", icon: Icons.postgresIcon },
+			{ tooltip: "Bootstrap", icon: Icons.bootstrapIcon },
+			{ tooltip: "RabbitMQ", icon: Icons.rabbitmqIcon },
+			{ tooltip: "JavaScript", icon: Icons.jsIcon },
+			{ tooltip: "Vue.js", icon: Icons.vuejsIcon },
+			{ tooltip: "Amazon Web Services (AWS)", icon: Icons.awsIcon },
+		],
+		projectTarget: "web",
+		media: [],
+		thumbnail: "thumbnail.jpg",
+		relatedWorkExperience: "Work Experience",
+		liveDemoLink: "",
+		isOpenSource: false,
+		openSourceRepo: "https://github.com/axisgreen",
+	},
+	{
+		id: "measureup",
+		projectName: "MeasureUp",
+		projectDescription: "desc",
+		technologiesUsed: [
+			{ tooltip: "Flutter", icon: Icons.flutterIcon },
+			{ tooltip: "Dart", icon: Icons.dartIcon },
+			{ tooltip: "Node.js", icon: Icons.nodejsIcon },
+			{ tooltip: "JavaScript", icon: Icons.jsIcon },
+		],
+		projectTarget: "mobile",
+		media: [],
+		thumbnail: "thumbnail.jpg",
+		relatedWorkExperience: "Work Experience",
+		liveDemoLink: "",
+		isOpenSource: false,
+		openSourceRepo: "https://github.com/axisgreen",
+	},
+	{
+		id: "iservice",
+		projectName: "iService",
+		projectDescription: "desc",
+		technologiesUsed: [
+			{ tooltip: "MongoDB", icon: Icons.mongodbIcon },
+			{ tooltip: "Express.js", icon: Icons.expressIcon },
+			{ tooltip: "React.js", icon: Icons.reactIcon },
+			{ tooltip: "Node.js", icon: Icons.nodejsIcon },
+			{ tooltip: "Stripe", icon: Icons.stripeIcon },
+			{ tooltip: "Heroku", icon: Icons.herokuIcon },
+			{ tooltip: "Google Cloud Platform (GCP)", icon: Icons.gcpIcon },
+		],
+		projectTarget: "web",
+		media: [],
+		thumbnail: "thumbnail.jpg",
+		relatedWorkExperience: "Work Experience",
+		liveDemoLink: "",
+		isOpenSource: false,
+		openSourceRepo: "https://github.com/axisgreen",
 	},
 ];
 
