6d621e6d19f598411c02e2f11b02230e4aa98597
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-01 02:00:19 +1100

    minor ui fix to tech badges

--- src/components/common/TechBadge.tsx
+++ src/components/common/TechBadge.tsx
@@ -12,7 +12,14 @@ const TechBadge: React.FC<Props> = ({ icon, tooltip }) => {
 				data-tip={tooltip}
 				className="bg-black opacity-75 bg-opacity-50 hover:bg-opacity-75 hover:opacity-100 transition-all rounded-md p-2"
 			>
-				<img src={icon} alt={tooltip} width="20px" height="20px" />
+				<img
+					src={icon}
+					alt={tooltip}
+					width="20px"
+					height="20px"
+					className="rounded-sm"
+					style={{ minHeight: "20px", minWidth: "20px" }}
+				/>
 			</div>
 			{/* Tooltip */}
 			<ReactTooltip place="bottom" effect="solid" backgroundColor="black" />
