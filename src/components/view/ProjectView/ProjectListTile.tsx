6a396af2e4865e9203226697ea97fa3e890ecc7c
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-01 02:03:23 +1100

    projectlisttile replaces previous list of proejcts with dynamic and animated approach

---
+++ src/components/view/ProjectView/ProjectListTile.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+import Projects from "../../../data/Projects";
+import Card from "../../common/Card";
+
+const ProjectListTile: React.FC = () => {
+	return (
+		<motion.div
+			initial={{ translateY: 40, opacity: 0 }}
+			animate={{
+				translateY: 0,
+				opacity: 1,
+			}}
+			transition={{ delay: 0.2 }}
+			className="flex flex-col flex-wrap bg-gray-800 shadow-xl rounded-md py-14 md:px-16 md:mx-16"
+		>
+			{Projects.map((project, i) => {
+				return (
+					<Card
+						key={i}
+						title={project.projectName}
+						description={project.projectDescription}
+						image={project.thumbnail}
+						link={`/projects/${project.id}`}
+					/>
+				);
+			})}
+		</motion.div>
+	);
+};
+
+export default ProjectListTile;
--- src/views/ProjectView.tsx
+++ src/views/ProjectView.tsx
@@ -1,27 +1,18 @@
 import Fade from "../components/transitions/PageFade";
 import Heading from "../components/common/Heading";
+import ProjectListTile from "../components/view/ProjectView/ProjectListTile";
 
 const ProjectView: React.FC = () => {
 	return (
 		<Fade>
-		<div className="flex flex-col flex-1 gap-8 text-white">
-			<div className="flex justify-center">
+			<div className="flex flex-col flex-1 gap-8 text-white">
+				<div className="flex justify-center">
 					<Heading variant="page">Projects</Heading>
-			<div className="mx-auto mb-4 xl:mb-0">
-				<div className="flex flex-col flex-wrap bg-gray-800 shadow-xl rounded-md py-14 md:px-16 md:mx-16">
-					{Projects.map((project, i) => {
-						return (
-							<Card
-								title={project.projectName}
-								description={project.projectDescription}
-								image={project.thumbnail}
-								link={`/projects/${project.id}`}
-							/>
-						);
-					})}
+				</div>
+				<div className="mx-auto mb-4 xl:mb-0 max-w-7xl">
+					<ProjectListTile />
 				</div>
 			</div>
-		</div>
 		</Fade>
 	);
 };
