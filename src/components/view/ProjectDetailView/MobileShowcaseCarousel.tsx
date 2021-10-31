94c9f4a4d0e5ee5f37c3d421ebd69cc0f94385a4
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-01 02:01:26 +1100

    animated web and mobile showcase components

--- src/components/view/ProjectDetailView/MobileShowcaseCarousel.tsx
+++ src/components/view/ProjectDetailView/MobileShowcaseCarousel.tsx
@@ -1,19 +1,31 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { OtherAssets } from "../../../assets/assets";
 
 const MobileShowcaseCarousel: React.FC = () => {
 	return (
-		<div className="flex justify-center items-center mb-8 sm:mb-0">
-			<img
-				src={OtherAssets.iphone}
-				alt="Mobile View"
-				style={{
-					minHeight: "40rem",
-					maxHeight: "75vh",
-					minWidth: "fit-content",
-				}}
-			/>
-		</div>
+		<motion.div
+			initial={{ translateY: 40, opacity: 0 }}
+			animate={{
+				translateY: 0,
+				opacity: 1,
+				animationFillMode: "both",
+			}}
+			transition={{ duration: 0.4 }}
+			className="flex justify-center items-center mb-8 sm:mb-0"
+		>
+			<div>
+				<img
+					src={OtherAssets.iphone}
+					alt="Mobile View"
+					style={{
+						minHeight: "40rem",
+						maxHeight: "75vh",
+						minWidth: "fit-content",
+					}}
+				/>
+			</div>
+		</motion.div>
 	);
 };
 
--- src/components/view/ProjectDetailView/WebShowcaseCarousel.tsx
+++ src/components/view/ProjectDetailView/WebShowcaseCarousel.tsx
@@ -1,18 +1,30 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { OtherAssets } from "../../../assets/assets";
 
 const WebShowcaseCarousel: React.FC = () => {
 	return (
-		<div className="flex justify-center items-center">
-			<img
-				src={OtherAssets.monitor}
-				alt="Web View"
-				style={{
-					minHeight: "30rem",
-					maxHeight: "50vh",
-				}}
-			/>
-		</div>
+		<motion.div
+			initial={{ translateY: 40, opacity: 0 }}
+			animate={{
+				translateY: 0,
+				opacity: 1,
+				animationFillMode: "both",
+			}}
+			transition={{ duration: 0.4 }}
+			className="flex justify-center items-center"
+		>
+			<div>
+				<img
+					src={OtherAssets.monitor}
+					alt="Web View"
+					style={{
+						minHeight: "30rem",
+						maxHeight: "50vh",
+					}}
+				/>
+			</div>
+		</motion.div>
 	);
 };
 
