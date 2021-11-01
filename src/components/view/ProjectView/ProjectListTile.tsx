da1c044168f841254831b3ef120dd1f7921d826d
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-01 22:00:08 +1100

    added staggered animation to project list

--- src/components/view/ProjectView/ProjectListTile.tsx
+++ src/components/view/ProjectView/ProjectListTile.tsx
@@ -11,17 +11,25 @@ const ProjectListTile: React.FC = () => {
 				opacity: 1,
 			}}
 			transition={{ delay: 0.2 }}
-			className="flex flex-col flex-wrap bg-gray-800 shadow-xl rounded-md py-14 md:px-16 md:mx-16"
+			className="flex flex-col flex-wrap bg-gray-800 shadow-xl rounded-md px-2 py-14 md:px-16 md:mx-16"
 		>
 			{Projects.map((project, i) => {
 				return (
-					<Card
-						key={i}
-						title={project.projectName}
-						description={project.projectDescription}
-						image={project.thumbnail}
-						link={`/projects/${project.id}`}
-					/>
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{
+							opacity: 1,
+						}}
+						transition={{ delay: 0.2 * i }}
+					>
+						<Card
+							key={i}
+							title={project.projectName}
+							description={project.projectDescription}
+							image={project.thumbnail}
+							link={`/projects/${project.id}`}
+						/>
+					</motion.div>
 				);
 			})}
 		</motion.div>
