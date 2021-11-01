5378cbf1e605268701e62364cc6e20929f6c2e68
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-01 22:02:49 +1100

    minor ui fix on mobile

--- src/views/ProjectDetailView.tsx
+++ src/views/ProjectDetailView.tsx
@@ -10,7 +10,7 @@ const ProjectDetailView: React.FC = () => {
 	const project = getProjectFromId(projectId);
 	return (
 		<Fade>
-			<div className="flex flex-1 flex-col lg:flex-row justify-center gap-8 3xl:gap-16 my-4 xl:mx-0 text-white">
+			<div className="flex flex-1 flex-col lg:flex-row justify-center sm:gap-8 3xl:gap-16 my-4 xl:mx-0 text-white">
 				{project && (
 					<>
 						{project.projectTarget === "mobile" ? (
