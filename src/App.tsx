106c0e74ca9ac2b8e4cebedcaf7efca98b2b12e8
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-02 00:18:32 +1100

    routes extracted to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css";
-import HomeView from "./views/HomeView";
-import { Route, Switch, useLocation } from "react-router-dom";
 import PageBackground from "./components/common/PageBackground";
 import { ViewportProvider } from "./contexts/ViewportContext";
-import ProjectView from "./views/ProjectView";
-import AboutView from "./views/AboutView";
-import ExperienceView from "./views/ExperienceView";
-import ProjectDetailView from "./views/ProjectDetailView";
 import Navigation from "./components/layout/Navigation/Navigation";
 import Footer from "./components/layout/Footer/Footer";
 import { AnimatePresence } from "framer-motion";
 import { Backgrounds } from "./assets/assets";
+import Routes from "./routes/Routes";
 
 function App() {
 	const location = useLocation();
@@ -20,23 +15,7 @@ function App() {
 				<div className="p-4 flex flex-col min-h-screen">
 					<Navigation />
 					<AnimatePresence exitBeforeEnter>
-						<Switch location={location} key={location.key}>
-							<Route exact path="/">
-								<HomeView />
-							</Route>
-							<Route exact path="/about">
-								<AboutView />
-							</Route>
-							<Route exact path="/projects">
-								<ProjectView />
-							</Route>
-							<Route exact path="/projects/:projectId">
-								<ProjectDetailView />
-							</Route>
-							<Route exact path="/experience">
-								<ExperienceView />
-							</Route>
-						</Switch>
+						<Routes />
 					</AnimatePresence>
 					<Footer />
 				</div>
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,31 @@
+import { Route, Switch, useLocation } from "react-router";
+import AboutView from "../views/AboutView";
+import ExperienceView from "../views/ExperienceView";
+import HomeView from "../views/HomeView";
+import ProjectDetailView from "../views/ProjectDetailView";
+import ProjectView from "../views/ProjectView";
+
+const Routes: React.FC = () => {
+	const location = useLocation();
+	return (
+		<Switch location={location} key={location.key}>
+			<Route exact path="/">
+				<HomeView />
+			</Route>
+			<Route exact path="/about">
+				<AboutView />
+			</Route>
+			<Route exact path="/projects">
+				<ProjectView />
+			</Route>
+			<Route exact path="/projects/:projectId">
+				<ProjectDetailView />
+			</Route>
+			<Route exact path="/experience">
+				<ExperienceView />
+			</Route>
+		</Switch>
+	);
+};
+
+export default Routes;
