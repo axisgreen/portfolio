ba43e54bbf46c388ad03658709a860c80fb73800
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-03 03:07:41 +1100

    minor refactor

--- src/views/ProjectDetailView.tsx
+++ src/views/ProjectDetailView.tsx
@@ -11,8 +11,6 @@ import { motion } from "framer-motion";
 const ProjectDetailView: React.FC = () => {
 	const { projectId } = useParams<{ projectId: string }>();
 	const project = getProjectFromId(projectId);
-
-	const mediaItems = [];
 	return (
 		<Fade>
 			<div className="flex flex-1 flex-col lg:flex-row justify-center gap-8 3xl:gap-16 my-4 xl:mx-0 text-white">
