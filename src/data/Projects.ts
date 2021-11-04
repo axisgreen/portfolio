d19c8f6739c62e288308612fb6ba2cf0800e2767
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-05 00:04:57 +1100

    added screenshots to servo and measureup

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -3,52 +3,52 @@ import { Icons, Thumbnails } from "../assets/assets";
 import ScreenshotGallery from "./ScreenshotGallery";
 
 type Technology = {
-  tooltip:
-    | "Flutter"
-    | "Bootstrap"
-    | "Django"
-    | "Python"
-    | "Express.js"
-    | "Bash"
-    | "MongoDB"
-    | "Node.js"
-    | "PostgreSQL"
-    | "RabbitMQ"
-    | "React.js"
-    | "TypeScript"
-    | "JavaScript"
-    | "jQuery"
-    | "Amazon Web Services (AWS)"
-    | "Google Cloud Platform (GCP)"
-    | "DigitalOcean"
-    | "Vue.js"
-    | "Dart"
-    | "Stripe"
-    | "Heroku";
-  icon: string;
+	tooltip:
+		| "Flutter"
+		| "Bootstrap"
+		| "Django"
+		| "Python"
+		| "Express.js"
+		| "Bash"
+		| "MongoDB"
+		| "Node.js"
+		| "PostgreSQL"
+		| "RabbitMQ"
+		| "React.js"
+		| "TypeScript"
+		| "JavaScript"
+		| "jQuery"
+		| "Amazon Web Services (AWS)"
+		| "Google Cloud Platform (GCP)"
+		| "DigitalOcean"
+		| "Vue.js"
+		| "Dart"
+		| "Stripe"
+		| "Heroku";
+	icon: string;
 };
 
 type ProjectTarget = "mobile" | "web" | "desktop";
 
 interface Project {
-  id: string;
-  projectName: string;
-  projectDescription: string;
-  technologiesUsed: Technology[];
-  projectTarget: ProjectTarget;
-  media: ReactImageGalleryItem[];
-  thumbnail: string;
-  relatedWorkExperience: string;
-  liveDemoLink: string;
-  isOpenSource: boolean;
-  openSourceRepo: string;
+	id: string;
+	projectName: string;
+	projectDescription: string;
+	technologiesUsed: Technology[];
+	projectTarget: ProjectTarget;
+	media: ReactImageGalleryItem[];
+	thumbnail: string;
+	relatedWorkExperience: string;
+	liveDemoLink: string;
+	isOpenSource: boolean;
+	openSourceRepo: string;
 }
 
 const Projects: Project[] = [
-  {
-    id: "aims",
-    projectName: "AIMS (Agent Information Management System)",
-    projectDescription: `Project Description - Lorem ipsum dolor sit amet consectetur
+	{
+		id: "aims",
+		projectName: "AIMS (Agent Information Management System)",
+		projectDescription: `Project Description - Lorem ipsum dolor sit amet consectetur
 		adipisicing elit. Nihil reiciendis ratione maxime doloribus
 		laborum. Incidunt minima quam in optio quas a laborum illo cumque
 		nesciunt, quis consequatur id, sed doloremque! Lorem ipsum dolor
@@ -63,103 +63,117 @@ const Projects: Project[] = [
 		voluptates aspernatur maxime, consequuntur optio ratione adipisci
 		commodi labore eaque, ea doloribus explicabo molestiae repellat
 		aut possimus cum eveniet. Consectetur, voluptatem.`,
-    technologiesUsed: [
-      { tooltip: "Python", icon: Icons.pythonIcon },
-      { tooltip: "Django", icon: Icons.djangoIcon },
-      { tooltip: "PostgreSQL", icon: Icons.postgresIcon },
-      { tooltip: "Bootstrap", icon: Icons.bootstrapIcon },
-      { tooltip: "JavaScript", icon: Icons.jsIcon },
-    ],
-    projectTarget: "web",
-    media: ScreenshotGallery.aims,
-    thumbnail: Thumbnails.aimsThumb,
-    relatedWorkExperience: "Work Experience",
-    liveDemoLink: "https://aims-demo.herokuapp.com/",
-    isOpenSource: false,
-    openSourceRepo: "https://github.com/axisgreen",
-  },
-  {
-    id: "servo",
-    projectName: "Servo",
-    projectDescription: "desc",
-    technologiesUsed: [
-      { tooltip: "Python", icon: Icons.pythonIcon },
-      { tooltip: "Django", icon: Icons.djangoIcon },
-      { tooltip: "PostgreSQL", icon: Icons.postgresIcon },
-      { tooltip: "Bootstrap", icon: Icons.bootstrapIcon },
-      { tooltip: "jQuery", icon: Icons.jqueryIcon },
-      { tooltip: "JavaScript", icon: Icons.jsIcon },
-      { tooltip: "DigitalOcean", icon: Icons.digitaloceanIcon },
-    ],
-    projectTarget: "web",
-    media: [{ original: "https://via.placeholder.com/250x200" }],
-    thumbnail: Thumbnails.servoThumb,
-    relatedWorkExperience: "Work Experience",
-    liveDemoLink: "https://servoapp-demo.herokuapp.com/",
-    isOpenSource: false,
-    openSourceRepo: "https://github.com/axisgreen",
-  },
-  {
-    id: "phantom",
-    projectName: "Phantom",
-    projectDescription: "desc",
-    technologiesUsed: [
-      { tooltip: "Python", icon: Icons.pythonIcon },
-      { tooltip: "Django", icon: Icons.djangoIcon },
-      { tooltip: "PostgreSQL", icon: Icons.postgresIcon },
-      { tooltip: "Bootstrap", icon: Icons.bootstrapIcon },
-      { tooltip: "RabbitMQ", icon: Icons.rabbitmqIcon },
-      { tooltip: "JavaScript", icon: Icons.jsIcon },
-      { tooltip: "Vue.js", icon: Icons.vuejsIcon },
-      { tooltip: "Amazon Web Services (AWS)", icon: Icons.awsIcon },
-    ],
-    projectTarget: "web",
-    media: ScreenshotGallery.phantom,
-    thumbnail: Thumbnails.phantomThumb,
-    relatedWorkExperience: "Work Experience",
-    liveDemoLink: "",
-    isOpenSource: false,
-    openSourceRepo: "https://github.com/axisgreen",
-  },
-  {
-    id: "measureup",
-    projectName: "MeasureUp",
-    projectDescription: "desc",
-    technologiesUsed: [
-      { tooltip: "Flutter", icon: Icons.flutterIcon },
-      { tooltip: "Dart", icon: Icons.dartIcon },
-      { tooltip: "Node.js", icon: Icons.nodejsIcon },
-      { tooltip: "JavaScript", icon: Icons.jsIcon },
-    ],
-    projectTarget: "mobile",
-    media: [{ original: "https://via.placeholder.com/250x200" }],
-    thumbnail: Thumbnails.measureupThumb,
-    relatedWorkExperience: "Work Experience",
-    liveDemoLink: "",
-    isOpenSource: false,
-    openSourceRepo: "https://github.com/axisgreen",
-  },
-  {
-    id: "iservice",
-    projectName: "iService",
-    projectDescription: "desc",
-    technologiesUsed: [
-      { tooltip: "MongoDB", icon: Icons.mongodbIcon },
-      { tooltip: "Express.js", icon: Icons.expressIcon },
-      { tooltip: "React.js", icon: Icons.reactIcon },
-      { tooltip: "Node.js", icon: Icons.nodejsIcon },
-      { tooltip: "Stripe", icon: Icons.stripeIcon },
-      { tooltip: "Heroku", icon: Icons.herokuIcon },
-      { tooltip: "Google Cloud Platform (GCP)", icon: Icons.gcpIcon },
-    ],
-    projectTarget: "web",
-    media: [{ original: "https://via.placeholder.com/250x200" }],
-    thumbnail: Thumbnails.iserviceThumb,
-    relatedWorkExperience: "Work Experience",
-    liveDemoLink: "",
-    isOpenSource: false,
-    openSourceRepo: "https://github.com/axisgreen",
-  },
+		technologiesUsed: [
+			{ tooltip: "Python", icon: Icons.pythonIcon },
+			{ tooltip: "Django", icon: Icons.djangoIcon },
+			{ tooltip: "PostgreSQL", icon: Icons.postgresIcon },
+			{ tooltip: "Bootstrap", icon: Icons.bootstrapIcon },
+			{ tooltip: "JavaScript", icon: Icons.jsIcon },
+		],
+		projectTarget: "web",
+		media: ScreenshotGallery.aims,
+		thumbnail: Thumbnails.aimsThumb,
+		relatedWorkExperience: "Work Experience",
+		liveDemoLink: "https://aims-demo.herokuapp.com/",
+		isOpenSource: false,
+		openSourceRepo: "https://github.com/axisgreen",
+	},
+	{
+		id: "servo",
+		projectName: "Servo",
+		projectDescription: "desc",
+		technologiesUsed: [
+			{ tooltip: "Python", icon: Icons.pythonIcon },
+			{ tooltip: "Django", icon: Icons.djangoIcon },
+			{ tooltip: "PostgreSQL", icon: Icons.postgresIcon },
+			{ tooltip: "Bootstrap", icon: Icons.bootstrapIcon },
+			{ tooltip: "jQuery", icon: Icons.jqueryIcon },
+			{ tooltip: "JavaScript", icon: Icons.jsIcon },
+			{ tooltip: "DigitalOcean", icon: Icons.digitaloceanIcon },
+		],
+		projectTarget: "web",
+		media: ScreenshotGallery.servo,
+		thumbnail: Thumbnails.servoThumb,
+		relatedWorkExperience: "Work Experience",
+		liveDemoLink: "https://servoapp-demo.herokuapp.com/",
+		isOpenSource: false,
+		openSourceRepo: "https://github.com/axisgreen",
+	},
+	{
+		id: "phantom",
+		projectName: "Phantom",
+		projectDescription: "desc",
+		technologiesUsed: [
+			{ tooltip: "Python", icon: Icons.pythonIcon },
+			{ tooltip: "Django", icon: Icons.djangoIcon },
+			{ tooltip: "PostgreSQL", icon: Icons.postgresIcon },
+			{ tooltip: "Bootstrap", icon: Icons.bootstrapIcon },
+			{ tooltip: "RabbitMQ", icon: Icons.rabbitmqIcon },
+			{ tooltip: "JavaScript", icon: Icons.jsIcon },
+			{ tooltip: "Vue.js", icon: Icons.vuejsIcon },
+			{ tooltip: "Amazon Web Services (AWS)", icon: Icons.awsIcon },
+		],
+		projectTarget: "web",
+		media: ScreenshotGallery.phantom,
+		thumbnail: Thumbnails.phantomThumb,
+		relatedWorkExperience: "Work Experience",
+		liveDemoLink: "",
+		isOpenSource: false,
+		openSourceRepo: "https://github.com/axisgreen",
+	},
+	{
+		id: "measureup",
+		projectName: "MeasureUp",
+		projectDescription: `Project Description - Lorem ipsum dolor sit amet consectetur
+		adipisicing elit. Nihil reiciendis ratione maxime doloribus
+		laborum. Incidunt minima quam in optio quas a laborum illo cumque
+		nesciunt, quis consequatur id, sed doloremque! Lorem ipsum dolor
+		sit amet, consectetur adipisicing elit. Incidunt voluptates
+		aspernatur maxime, consequuntur optio ratione adipisci commodi
+		labore eaque, ea doloribus explicabo molestiae repellat aut
+		possimus cum eveniet. Consectetur, voluptatem. Lorem ipsum dolor
+		sit amet consectetur adipisicing elit. Nihil reiciendis ratione
+		maxime doloribus laborum. Incidunt minima quam in optio quas a
+		laborum illo cumque nesciunt, quis consequatur id, sed doloremque!
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
+		voluptates aspernatur maxime, consequuntur optio ratione adipisci
+		commodi labore eaque, ea doloribus explicabo molestiae repellat
+		aut possimus cum eveniet. Consectetur, voluptatem.`,
+		technologiesUsed: [
+			{ tooltip: "Flutter", icon: Icons.flutterIcon },
+			{ tooltip: "Dart", icon: Icons.dartIcon },
+			{ tooltip: "Node.js", icon: Icons.nodejsIcon },
+			{ tooltip: "JavaScript", icon: Icons.jsIcon },
+		],
+		projectTarget: "mobile",
+		media: ScreenshotGallery.measureup,
+		thumbnail: Thumbnails.measureupThumb,
+		relatedWorkExperience: "Work Experience",
+		liveDemoLink: "",
+		isOpenSource: false,
+		openSourceRepo: "https://github.com/axisgreen",
+	},
+	{
+		id: "iservice",
+		projectName: "iService",
+		projectDescription: "desc",
+		technologiesUsed: [
+			{ tooltip: "MongoDB", icon: Icons.mongodbIcon },
+			{ tooltip: "Express.js", icon: Icons.expressIcon },
+			{ tooltip: "React.js", icon: Icons.reactIcon },
+			{ tooltip: "Node.js", icon: Icons.nodejsIcon },
+			{ tooltip: "Stripe", icon: Icons.stripeIcon },
+			{ tooltip: "Heroku", icon: Icons.herokuIcon },
+			{ tooltip: "Google Cloud Platform (GCP)", icon: Icons.gcpIcon },
+		],
+		projectTarget: "web",
+		media: [{ original: "https://via.placeholder.com/250x200" }],
+		thumbnail: Thumbnails.iserviceThumb,
+		relatedWorkExperience: "Work Experience",
+		liveDemoLink: "",
+		isOpenSource: false,
+		openSourceRepo: "https://github.com/axisgreen",
+	},
 ];
 
 export interface IProject extends Project {}
--- src/data/ScreenshotGallery.ts
+++ src/data/ScreenshotGallery.ts
@@ -2,139 +2,211 @@ import { ReactImageGalleryItem } from "react-image-gallery";
 import { Screenshots } from "../assets/assets";
 
 interface Gallery {
-  [project: string]: ReactImageGalleryItem[];
+	[project: string]: ReactImageGalleryItem[];
 }
 
 const ScreenshotGallery: Gallery = {
-  aims: [
-    {
-      original: Screenshots.aims.aimsDashboardStaff,
-      description: "Dashboard for Administrators and Internal Staff",
-    },
-    {
-      original: Screenshots.aims.aimsActivations,
-      description: "KPI and target tracking for employed agents",
-    },
-    {
-      original: Screenshots.aims.aimsAddActivation,
-      description: "KPI and target tracking for employed agents",
-    },
-    { original: Screenshots.aims.aimsAgents, description: "Agent Management" },
-    {
-      original: Screenshots.aims.aimsAddAgent,
-      description: "Agent Management",
-    },
-    {
-      original: Screenshots.aims.aimsAgentSalaries,
-      description: "Agent Salary/Payroll Creation and Management",
-    },
-    {
-      original: Screenshots.aims.aimsAddAgentSalary,
-      description: "Agent Salary/Payroll Creation and Management",
-    },
-    {
-      original: Screenshots.aims.aimsAddSalaryAddition,
-      description: "Agent Salary/Payroll Creation and Management",
-    },
-    {
-      original: Screenshots.aims.aimsSalaryAddition,
-      description: "Agent Salary/Payroll Creation and Management",
-    },
-    {
-      original: Screenshots.aims.aimsSalaryDeductions,
-      description: "Agent Salary/Payroll Creation and Management",
-    },
-    {
-      original: Screenshots.aims.aimsSalarySummary,
-      description: "Agent Salary/Payroll Creation and Management",
-    },
-    {
-      original: Screenshots.aims.aimsUsers,
-      description: "User administration and permission control",
-    },
-    {
-      original: Screenshots.aims.aimsEditUser,
-      description: "User administration and permission control",
-    },
-    {
-      original: Screenshots.aims.aimsUserPermissions,
-      description: "User administration and permission control",
-    },
-    {
-      original: Screenshots.aims.aimsReportPayroll,
-      description: "Report Generation and Data Processing",
-    },
-    {
-      original: Screenshots.aims.aimsReportPayrollExample,
-      description: "Report Generation and Data Processing",
-    },
-    {
-      original: Screenshots.aims.aimsReportAgent,
-      description: "Report Generation and Data Processing",
-    },
-    {
-      original: Screenshots.aims.aimsReportAgentExample,
-      description: "Report Generation and Data Processing",
-    },
-    {
-      original: Screenshots.aims.aimsDashboardEmployee,
-      description: "Employee/Agent version of the application",
-    },
-    {
-      original: Screenshots.aims.aimsReportActivationEmployee,
-      description: "Employee/Agent version of the application",
-    },
-    {
-      original: Screenshots.aims.aimsReportSalaryEmployee,
-      description: "Employee/Agent version of the application",
-    },
-    {
-      original: Screenshots.aims.aimsSalaryEmployeeExample,
-      description: "Employee/Agent version of the application",
-    },
-    {
-      original: Screenshots.aims.aimsEmployeeAccEdit,
-      description: "Employee/Agent version of the application",
-    },
-  ],
-  phantom: [
-    {
-      original: Screenshots.phantom.phantomDashboard,
-      description: "Dashboard",
-    },
-    {
-      original: Screenshots.phantom.phantomManageNews,
-      description: "Manage News Items (Approve/Reject/Edit)",
-    },
-    {
-      original: Screenshots.phantom.phantomAddNews,
-      description: "Add a News Item to send",
-    },
-    {
-      original: Screenshots.phantom.phantomAccounts,
-      description: "Accounts Overview",
-    },
-    {
-      original: Screenshots.phantom.phantomManageAccount,
-      description: "Manage an account",
-    },
-    {
-      original: Screenshots.phantom.phantomAddApp,
-      description: "Add an application to the account",
-    },
-    {
-      original: Screenshots.phantom.phantomAccMsgHistory,
-      description: "Account Messaging History",
-    },
-    {
-      original: Screenshots.phantom.phantomEventLog,
-      description: "Event Logs",
-    },
-    {
-      original: Screenshots.phantom.phantomSettings,
-      description: "Settings",
-    },
-  ],
+	aims: [
+		{
+			original: Screenshots.aims.aimsDashboardStaff,
+			description: "Dashboard for Administrators and Internal Staff",
+		},
+		{
+			original: Screenshots.aims.aimsActivations,
+			description: "KPI and target tracking for employed agents",
+		},
+		{
+			original: Screenshots.aims.aimsAddActivation,
+			description: "KPI and target tracking for employed agents",
+		},
+		{ original: Screenshots.aims.aimsAgents, description: "Agent Management" },
+		{
+			original: Screenshots.aims.aimsAddAgent,
+			description: "Agent Management",
+		},
+		{
+			original: Screenshots.aims.aimsAgentSalaries,
+			description: "Agent Salary/Payroll Creation and Management",
+		},
+		{
+			original: Screenshots.aims.aimsAddAgentSalary,
+			description: "Agent Salary/Payroll Creation and Management",
+		},
+		{
+			original: Screenshots.aims.aimsAddSalaryAddition,
+			description: "Agent Salary/Payroll Creation and Management",
+		},
+		{
+			original: Screenshots.aims.aimsSalaryAddition,
+			description: "Agent Salary/Payroll Creation and Management",
+		},
+		{
+			original: Screenshots.aims.aimsSalaryDeductions,
+			description: "Agent Salary/Payroll Creation and Management",
+		},
+		{
+			original: Screenshots.aims.aimsSalarySummary,
+			description: "Agent Salary/Payroll Creation and Management",
+		},
+		{
+			original: Screenshots.aims.aimsUsers,
+			description: "User administration and permission control",
+		},
+		{
+			original: Screenshots.aims.aimsEditUser,
+			description: "User administration and permission control",
+		},
+		{
+			original: Screenshots.aims.aimsUserPermissions,
+			description: "User administration and permission control",
+		},
+		{
+			original: Screenshots.aims.aimsReportPayroll,
+			description: "Report Generation and Data Processing",
+		},
+		{
+			original: Screenshots.aims.aimsReportPayrollExample,
+			description: "Report Generation and Data Processing",
+		},
+		{
+			original: Screenshots.aims.aimsReportAgent,
+			description: "Report Generation and Data Processing",
+		},
+		{
+			original: Screenshots.aims.aimsReportAgentExample,
+			description: "Report Generation and Data Processing",
+		},
+		{
+			original: Screenshots.aims.aimsDashboardEmployee,
+			description: "Employee/Agent version of the application",
+		},
+		{
+			original: Screenshots.aims.aimsReportActivationEmployee,
+			description: "Employee/Agent version of the application",
+		},
+		{
+			original: Screenshots.aims.aimsReportSalaryEmployee,
+			description: "Employee/Agent version of the application",
+		},
+		{
+			original: Screenshots.aims.aimsSalaryEmployeeExample,
+			description: "Employee/Agent version of the application",
+		},
+		{
+			original: Screenshots.aims.aimsEmployeeAccEdit,
+			description: "Employee/Agent version of the application",
+		},
+	],
+	phantom: [
+		{
+			original: Screenshots.phantom.phantomDashboard,
+			description: "Dashboard",
+		},
+		{
+			original: Screenshots.phantom.phantomManageNews,
+			description: "Manage News Items (Approve/Reject/Edit)",
+		},
+		{
+			original: Screenshots.phantom.phantomAddNews,
+			description: "Add a News Item to send",
+		},
+		{
+			original: Screenshots.phantom.phantomAccounts,
+			description: "Accounts Overview",
+		},
+		{
+			original: Screenshots.phantom.phantomManageAccount,
+			description: "Manage an account",
+		},
+		{
+			original: Screenshots.phantom.phantomAddApp,
+			description: "Add an application to the account",
+		},
+		{
+			original: Screenshots.phantom.phantomAccMsgHistory,
+			description: "Account Messaging History",
+		},
+		{
+			original: Screenshots.phantom.phantomEventLog,
+			description: "Event Logs",
+		},
+		{
+			original: Screenshots.phantom.phantomSettings,
+			description: "Settings",
+		},
+	],
+	servo: [
+		{
+			original: Screenshots.servo.servoDashboard,
+			description: "Dashboard",
+		},
+		{
+			original: Screenshots.servo.servoCreateNew,
+			description: "Create New Customer/Vehicle/Service Record",
+		},
+		{
+			original: Screenshots.servo.servoCustomers,
+			description: "Customer Information Management",
+		},
+		{
+			original: Screenshots.servo.servoNewCustomer,
+			description: "Customer Information Management",
+		},
+		{
+			original: Screenshots.servo.servoCustomerDetail,
+			description: "Customer Information Management",
+		},
+		{
+			original: Screenshots.servo.servoVehicles,
+			description: "Vehicle Management",
+		},
+		{
+			original: Screenshots.servo.servoNewVehicle,
+			description: "Vehicle Management",
+		},
+		{
+			original: Screenshots.servo.servoVehicleDetail,
+			description: "Vehicle Management",
+		},
+		{
+			original: Screenshots.servo.servoServices,
+			description: "Service Record Management",
+		},
+		{
+			original: Screenshots.servo.servoNewService,
+			description: "Service Record Management",
+		},
+		{
+			original: Screenshots.servo.servoServiceDetail,
+			description: "Service Record Management",
+		},
+		{
+			original: Screenshots.servo.servoUsers,
+			description: "User Management and Permission Control",
+		},
+		{
+			original: Screenshots.servo.servoEditUser,
+			description: "User Management and Permission Control",
+		},
+		{
+			original: Screenshots.servo.servoUserLogs,
+			description: "User Management and Permission Control",
+		},
+		{
+			original: Screenshots.servo.servoExport,
+			description: "Report Generation and Export",
+		},
+		{
+			original: Screenshots.servo.servoExportExample,
+			description: "Report Generation and Export",
+		},
+	],
+	measureup: [
+		{ original: Screenshots.measureup.measureupScreen1 },
+		{ original: Screenshots.measureup.measureupScreen2 },
+		{ original: Screenshots.measureup.measureupScreen3 },
+		{ original: Screenshots.measureup.measureupScreen4 },
+	],
 };
 
 export default ScreenshotGallery;
