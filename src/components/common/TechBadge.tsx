e441105662b1dca933970d23c4523d944d04c20a
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-05 00:02:54 +1100

    minor ui update

--- src/components/common/TechBadge.tsx
+++ src/components/common/TechBadge.tsx
@@ -1,30 +1,30 @@
 import ReactTooltip from "react-tooltip";
 
 interface Props {
-  icon: string;
-  tooltip: string;
+	icon: string;
+	tooltip: string;
 }
 
 const TechBadge: React.FC<Props> = ({ icon, tooltip }) => {
-  return (
-    <div>
-      <div
-        data-tip={tooltip}
-        className="bg-black bg-opacity-50 hover:bg-opacity-75 transition-all rounded-md p-2"
-      >
-        <img
-          src={icon}
-          alt={tooltip}
-          width="20px"
-          height="20px"
-          className="rounded-sm"
-          style={{ minHeight: "20px", minWidth: "20px" }}
-        />
-      </div>
-      {/* Tooltip */}
-      <ReactTooltip place="bottom" effect="solid" backgroundColor="black" />
-    </div>
-  );
+	return (
+		<div>
+			<div
+				data-tip={tooltip}
+				className="bg-black bg-opacity-25 hover:bg-opacity-75 transition-all rounded-md p-2"
+			>
+				<img
+					src={icon}
+					alt={tooltip}
+					width="20px"
+					height="20px"
+					className="rounded-sm"
+					style={{ minHeight: "20px", minWidth: "20px" }}
+				/>
+			</div>
+			{/* Tooltip */}
+			<ReactTooltip place="bottom" effect="solid" backgroundColor="black" />
+		</div>
+	);
 };
 
 export default TechBadge;
