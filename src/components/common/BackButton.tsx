fd6afa39bb1903d11d41126e6c679936e56f71ee
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-05 00:02:41 +1100

    added border to back button component

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -3,24 +3,24 @@ import { Icons } from "../../assets/assets";
 import "./BackButton.css";
 
 const BackButton: React.FC = () => {
-  const history = useHistory();
-  return (
-    <button
-      className="rounded-md p-2 hover:shadow-lg hover:bg-white hover:text-black transition-all chevron-parent"
-      onClick={() => history.goBack()}
-    >
-      <div className="flex gap-2 items-center">
-        <img
-          src={Icons.chevronLeft}
-          alt="Back"
-          width="10px"
-          height="10px"
-          className="chevron-left"
-        />
-        Back
-      </div>
-    </button>
-  );
+	const history = useHistory();
+	return (
+		<button
+			className="rounded-md p-2 hover:shadow-lg border hover:bg-white hover:text-black transition-all chevron-parent"
+			onClick={() => history.goBack()}
+		>
+			<div className="flex gap-2 items-center">
+				<img
+					src={Icons.chevronLeft}
+					alt="Back"
+					width="10px"
+					height="10px"
+					className="chevron-left"
+				/>
+				Back
+			</div>
+		</button>
+	);
 };
 
 export default BackButton;
