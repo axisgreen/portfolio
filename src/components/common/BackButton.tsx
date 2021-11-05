c65fd992276773d622e1a1f90b70ef16dce51850
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-05 20:36:49 +1100

    adjusted back button appearance

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -6,18 +6,21 @@ const BackButton: React.FC = () => {
 	const history = useHistory();
 	return (
 		<button
-			className="rounded-md p-2 hover:shadow-lg border hover:bg-white hover:text-black transition-all chevron-parent"
+			className="rounded-md p-2 hover:shadow-lg border hover:bg-white hover:text-black transition-all chevron-parent text-xs"
 			onClick={() => history.goBack()}
 		>
-			<div className="flex gap-2 items-center">
-				<img
-					src={Icons.chevronLeft}
-					alt="Back"
-					width="10px"
-					height="10px"
-					className="chevron-left"
-				/>
-				Back
+			<div className="flex gap-2">
+				<div className="flex items-center">
+					<img
+						src={Icons.chevronLeft}
+						alt="Back"
+						width="7px"
+						height="7px"
+						className="chevron-left"
+						style={{ paddingTop: "1px", paddingBottom: "1px" }}
+					/>
+				</div>
+				<div className="items-center justify-center">Back</div>
 			</div>
 		</button>
 	);
