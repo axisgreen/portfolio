d0b913b6d59584995832d90ff0ddaf41736e2e05
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-05 22:13:34 +1100

    added scroll to top to project info view

--- src/views/ProjectDetailView.tsx
+++ src/views/ProjectDetailView.tsx
@@ -5,12 +5,14 @@ import ProjectInfoTile from "../components/view/ProjectDetailView/ProjectInfoTil
 import getProjectFromId from "../helpers/getProjectFromId";
 import "react-image-gallery/styles/css/image-gallery.css";
 import { motion } from "framer-motion";
+import ScrollToTop from "../components/utility/ScrollToTop";
 
 const ProjectDetailView: React.FC = () => {
 	const { projectId } = useParams<{ projectId: string }>();
 	const project = getProjectFromId(projectId);
 	return (
 		<Fade>
+			<ScrollToTop />
 			<div className="flex flex-1 flex-col lg:flex-row justify-center items-center gap-8 3xl:gap-16 my-4 xl:mx-0 text-white">
 				{project && (
 					<>
