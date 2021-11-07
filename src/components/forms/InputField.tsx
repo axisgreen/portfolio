150fb9a9b0f0942c66b4c8bb5486c7f679d476ed
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-08 00:08:51 +1100

    new form component inputfield

---
+++ src/components/forms/InputField.tsx
@@ -0,0 +1,33 @@
+import { HTMLInputTypeAttribute } from "react";
+
+interface Props {
+	name: string;
+	type?: HTMLInputTypeAttribute;
+	isTextArea?: boolean;
+	label: string;
+}
+
+const InputField: React.FC<Props> = ({ name, type, isTextArea, label }) => {
+	return (
+		<div className="flex flex-col gap-2">
+			<label htmlFor={name} className="font-semibold">
+				{label}
+			</label>
+			{!isTextArea ? (
+				<input
+					type={type || "text"}
+					name={name}
+					className="rounded-sm w-full p-2 bg-gray-900 focus:bg-white transition-all text-black"
+				/>
+			) : (
+				<textarea
+					name={name}
+					className="rounded-sm w-full p-2 bg-gray-900 focus:bg-white transition-all text-black"
+					rows={10}
+				></textarea>
+			)}
+		</div>
+	);
+};
+
+export default InputField;
