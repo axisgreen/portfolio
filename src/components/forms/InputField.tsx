da80a2b985cc1097cd76447be1ed4c9956681ffa
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-08 00:21:40 +1100

    field text color changes based on focus

--- src/components/forms/InputField.tsx
+++ src/components/forms/InputField.tsx
@@ -17,12 +17,12 @@ const InputField: React.FC<Props> = ({ name, type, isTextArea, label }) => {
 				<input
 					type={type || "text"}
 					name={name}
-					className="rounded-sm w-full p-2 bg-gray-900 focus:bg-white transition-all text-black"
+					className="rounded-sm w-full p-2 bg-gray-900 focus:bg-white transition-all focus:text-black text-white"
 				/>
 			) : (
 				<textarea
 					name={name}
-					className="rounded-sm w-full p-2 bg-gray-900 focus:bg-white transition-all text-black"
+					className="rounded-sm w-full p-2 bg-gray-900 focus:bg-white transition-all focus:text-black"
 					rows={10}
 				></textarea>
 			)}
