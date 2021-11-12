d8fcd12cfdf5a37f60b472f3209dc8f0a04ac743
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-12 19:25:23 +1100

    converted motion.divs experiencestile to fadeup staggeredfadein

--- src/components/view/ExperienceView/ExperiencesTile.tsx
+++ src/components/view/ExperienceView/ExperiencesTile.tsx
@@ -1,30 +1,16 @@
-import { motion } from "framer-motion";
 import Experiences from "../../../data/Experiences";
 import JobExpandable from "../../common/JobExpandable";
 import Spacer from "../../common/Spacer";
+import FadeUp from "../../framer/FadeUp";
+import StaggeredFadeIn from "../../framer/StaggeredFadeIn";
 
 const ExperiencesTile: React.FC = () => {
 	return (
-		<motion.div
-			initial={{ translateY: 40, opacity: 0 }}
-			animate={{
-				translateY: 0,
-				opacity: 1,
-			}}
-			transition={{ delay: 0.2 }}
-			className="flex flex-wrap bg-gray-800 shadow-xl rounded-md px-4 py-10 md:px-16 md:mx-16 justify-center"
-		>
+		<FadeUp className="flex flex-wrap bg-gray-800 shadow-xl rounded-md px-4 py-10 md:px-16 md:mx-16 justify-center">
 			<div>
 				{Experiences.map((experience, i) => {
 					return (
-						<motion.div
-							key={i}
-							initial={{ opacity: 0 }}
-							animate={{
-								opacity: 1,
-							}}
-							transition={{ delay: 0.15 * i }}
-						>
+						<StaggeredFadeIn i={i} transitionDelay={0.15}>
 							<JobExpandable
 								key={i}
 								jobTitle={experience.jobTitle}
@@ -37,11 +23,11 @@ const ExperiencesTile: React.FC = () => {
 							/>
 							{/* Don't put the spacer after the last item */}
 							{Experiences.length - 1 > i && <Spacer />}
-						</motion.div>
+						</StaggeredFadeIn>
 					);
 				})}
 			</div>
-		</motion.div>
+		</FadeUp>
 	);
 };
 
