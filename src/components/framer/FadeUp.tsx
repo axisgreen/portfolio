21852f28333e77b56d22bd27e9851b2ea4dd0945
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-12 18:35:01 +1100

    new framer component for fadeup effect

---
+++ src/components/framer/FadeUp.tsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+
+interface Props {
+	transitionDelay: number;
+	className: string;
+}
+
+const FadeUp: React.FC<Props> = ({
+	transitionDelay = 0.2,
+	className,
+	children,
+}) => {
+	return (
+		<motion.div
+			initial={{ translateY: 40, opacity: 0 }}
+			animate={{
+				translateY: 0,
+				opacity: 1,
+			}}
+			transition={{ delay: transitionDelay }}
+			className={className}
+		>
+			{children}
+		</motion.div>
+	);
+};
+
+export default FadeUp;
