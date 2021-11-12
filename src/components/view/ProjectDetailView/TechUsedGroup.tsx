e0ddbf9ec90b5696f9859546b2aacb665a9ff248
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-12 19:25:49 +1100

    techusedgroup refactored to staggeredfadein

--- src/components/view/ProjectDetailView/TechUsedGroup.tsx
+++ src/components/view/ProjectDetailView/TechUsedGroup.tsx
@@ -1,7 +1,7 @@
-import { motion } from "framer-motion";
 import React from "react";
 import { ITechnology } from "../../../data/Projects";
 import TechBadge from "../../common/TechBadge";
+import StaggeredFadeIn from "../../framer/StaggeredFadeIn";
 
 interface Props {
 	technologiesUsed: ITechnology[];
@@ -12,18 +12,13 @@ const TechUsedGroup: React.FC<Props> = ({ technologiesUsed }) => {
 		<div className="flex gap-1">
 			{technologiesUsed.map((technology, i) => {
 				return (
-					<motion.div
-						key={i}
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						transition={{ delay: 0.15 * i }}
-					>
+					<StaggeredFadeIn i={i} transitionDelay={0.1}>
 						<TechBadge
 							key={i}
 							icon={technology.icon}
 							tooltip={technology.tooltip}
 						/>
-					</motion.div>
+					</StaggeredFadeIn>
 				);
 			})}
 		</div>
