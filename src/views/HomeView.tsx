9211b4c7343e4da41b0d6b02dae639e635766807
axisgreen <54710277+axisgreen@users.noreply.github.com>
2021-11-12 23:15:56 +1100

    typewriter effect extracted to new component

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import PrimaryButton from "../components/common/PrimaryButton";
 import SecondaryButton from "../components/common/SecondaryButton";
 import ExoTwoFont from "../components/fonts/ExoTwoFont";
-import Typist from "react-typist";
 import "react-typist/dist/Typist.css";
 import PageFade from "../components/transitions/PageFade";
 import { useHistory } from "react-router";
+import TypewriterSubtitle from "../components/view/HomeView/TypewriterSubtitle";
 
 const HomeView: React.FC = () => {
 	const history = useHistory();
@@ -18,23 +18,7 @@ const HomeView: React.FC = () => {
 							Haritha Dissanayake
 						</div>
 					</ExoTwoFont>
-					<div className="mx-1 py-4 text-xl sm:text-2xl lg:text-4xl font-mono ">
-						<div className="flex">
-							<Typist
-								cursor={{
-									show: true,
-									blink: true,
-								}}
-							>
-								&lt;Web
-								<Typist.Backspace count={"Web".length} delay={1000} />
-								Mobile
-								<Typist.Backspace count={"Mobile".length} delay={1000} />
-								Full-stack Developer /&gt;
-							</Typist>
-						</div>
-					</div>
-
+					<TypewriterSubtitle />
 					<div className="flex gap-2 my-2 mx-1">
 						<PrimaryButton
 							buttonText="Get in Touch"
